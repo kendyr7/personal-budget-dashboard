9bf28af45f53bf9d0166ad0bf3a1adfcbe79505d
Kendyr <kendyr7@gmail.com>
2023-11-16 03:24:43 -0600

    Trying to collapse left sidebar (pending to fix), adding a simplified hover for sidebar / Changing navigation list for phone view

--- assets/main.js
+++ assets/main.js
@@ -35,3 +35,34 @@ hideButton.addEventListener('click', function() {
     secondaryContainer.classList.toggle('hidden');
     mainContainer.classList.toggle('expanded');
 });
+
+
+
+// SIDEBAR
+
+// JavaScript to toggle sidebar state
+const expand_btn = document.querySelector(".expand-btn");
+
+let activeIndex;
+
+expand_btn.addEventListener("click", () => {
+    document.body.classList.toggle("collapsed");
+});
+
+const current = window.location.href;
+
+const allLinks = document.querySelectorAll(".sidebar-items a");
+
+allLinks.forEach((elem) => {
+    elem.addEventListener("click", function () {
+        const hrefLinkClick = elem.href;
+
+        allLinks.forEach((link) => {
+            if (link.href === hrefLinkClick) {
+                link.classList.add("active");
+            } else {
+                link.classList.remove("active");
+            }
+        });
+    });
+});
